03db96c652eb3b62cc59115cfb9c19103e40d047
haxsampo <43881588+haxsampo@users.noreply.github.com>
2019-03-19 12:58:44 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,27 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
-
-class Item:
-    def __init__(self, name):
-        self.name = name
-
-nimi = "Essi Esimerkki"
-
-lista = [1, 1, 2, 3, 5, 8, 11]
-
-esineet = []
-esineet.append(Item("Eka"))
-esineet.append(Item("Toka"))
-esineet.append(Item("Kolmas"))
-esineet.append(Item("Neljäs"))
   
 @app.route("/")
 def hello():
-    return render_template("index.html")
-
-@app.route("/demo")
-def content():
-    return render_template("demo.html", nimi=nimi, lista=lista, esineet=esineet)
+    return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
